39cef210d31883ea78f5e075ceffd92dfcdb1797
Liam Schauerman <ljschauerman@gmail.com>
2014-08-24 19:47:59 -0700

    Complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
-  // your code here
+var getElementsByClassName = function(className, element, results){
+	var results = results || [];
+	var element = element || document.body;
+
+	if (element.classList.contains(className)){
+		results.push(element)
+	}
+
+	if(element.children.length>0){
+		for (var i=0; i<element.children.length; i++){
+			var nextElement = element.children[i];
+			getElementsByClassName(className, nextElement, results);
+		}
+	}
+	return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,68 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+// console.log(JSON.stringify(obj)+"   <<--argument being passed")
+
+	if (!str) {
+		var str = "";
+	}
+
+	// The argument is a number, null, or boolean 
+	// Argument gets wrapped in quotes, added to str, and returned
+	if(typeof obj === 'number'){
+		str = str+obj.toString();
+	};
+	if(obj === null) {
+		str += 'null'
+	};
+	if(typeof obj === 'boolean'){
+		if(obj){
+			str += 'true'
+		} else{
+			str += 'false'
+		}
+	}
+
+	// The argument is a string, add it to str
+	if(typeof obj === "string"){
+		str = str+'"'+obj+'"'
+	}
+	var objType = toString.call(obj);
+
+	// If the argument is an array
+	if(Array.isArray(obj)){
+		str += "["
+		var toAdd = []
+		for(var i =0; i < obj.length ; i++){
+			toAdd.push(stringifyJSON(obj[i]))
+		}
+		str += toAdd.join(',')
+		str += "]"
+	}
+
+	// If the argument is an ojbect, but not null or an array
+	if(typeof obj === 'object' && obj !== null && !Array.isArray(obj)){
+		str += "{"
+		var toAdd = [];
+		// Creates an array, one element for each keyVal pair, then joins them and adds them to str
+		for(var key in obj){ 
+			if(obj[key] === undefined) continue;
+			if(typeof obj[key] === 'function') continue;
+			var keyVal = "";
+			keyVal+= '"'+key+'":';
+			var value = stringifyJSON(obj[key]);
+			keyVal += value;
+			toAdd.push(keyVal);
+		}
+		str += toAdd.join(',')
+		str += "}"
+	}
+	return str;	
 };
+
+
+
+
